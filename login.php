54a8372541c659b5453d453d87290b8c88b29f0b
Gami13 <gamiofficial0@gmail.com>
2021-10-14 15:53:00 +0200

    wyswietlanie uwag i inne

--- login.php
+++ login.php
@@ -1,38 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-    <title>Document</title>
-</head>
-
-<body>
     <?php
     include_once("header.php");
     ?>
     <link href="http://fonts.cdnfonts.com/css/honey-script" rel="stylesheet">
-    <img class="bombel" src="bombel.png" alt="">
-    <img class="heksagon"src="heksagon.svg" alt="">
-        <div class="container">
-            <div class="logowanie">  
-                    <h1 class="h1Default">Logowanie</h1>
-                    <h2>Proszę podać e-mail i hasło</h2>
-                    <div class="logowanieDiv">
-                        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                            <label for="email">E-mail:</label>
-                            <input class="input" type="text" name="email" required><br>
-                            <label for="Password">Hasło:</label>
-                            <input type="password" name="Password" required><br>
-                         </div>
-                             <div class="log">
-                                 <input type="submit" value="Zaloguj!">
-                             </div>
-                     </form>
-                
+    <img class="bombel" src="img/bombel.png" alt="">
+    <img class="heksagon" src="img/heksagon.svg" alt="">
+    <div class="container">
+        <div class="logowanie">
+            <h1 class="h1Default">Logowanie</h1>
+            <h2>Proszę podać e-mail i hasło</h2>
+            <div class="logowanieDiv">
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <label for="email">E-mail:</label>
+                    <input class="input" type="text" name="email" required><br>
+                    <label for="Password">Hasło:</label>
+                    <input type="password" name="password" required><br>
             </div>
+            <div class="log">
+                <input type="submit" value="Zaloguj!">
+            </div>
+            </form>
+
         </div>
+    </div>
     </div>
     </div>
 
@@ -42,33 +32,42 @@
 
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
 
-    if (empty($email)) {
+    if (empty($email))
+    {
         $error = $error . "Email can't be empty!";
     }
 
-    if (empty($password)) {
+    if (empty($password))
+    {
         $error = $error . "Password can't be empty!";
     }
 
-    if (empty($error)) {
+    if (empty($error))
+    {
         $sql = "SELECT userId, userEmail, userPassword, userRank FROM users WHERE userEmail = ?";
 
-        if ($stmt = $mysqli->prepare($sql)) {
+        if ($stmt = $mysqli->prepare($sql))
+        {
             $stmt->bind_param("s", $param_email);
             $param_email = $email;
 
-            if ($stmt->execute()) {
+            if ($stmt->execute())
+            {
                 $stmt->store_result();
 
-                if ($stmt->num_rows == 1) {
+                if ($stmt->num_rows == 1)
+                {
                     $stmt->bind_result($userId, $userEmail, $userPasswordHashed, $userRank); //Nazwa z sqla
 
-                    if ($stmt->fetch()) {
-                        if (password_verify($password, $userPasswordHashed)) {
+                    if ($stmt->fetch())
+                    {
+                        if (password_verify($password, $userPasswordHashed))
+                        {
                             $_SESSION["loggedIn"] = true;
                             $_SESSION["id"] = $userId;
                             $_SESSION["email"] = $userEmail;
@@ -76,19 +75,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             $_SESSION["rank"] = $obj->{'rank'};
 
 
-                            if (in_array("uczen", $_SESSION["rank"])) {
+                            if (in_array("uczen", $_SESSION["rank"]))
+                            {
                                 header("location: member.php");
-                            } else {
+                            }
+                            else
+                            {
                                 echo "nauczyciel";
                             }
-                        } else {
+                        }
+                        else
+                        {
                             $error = $error . "Invalid username or password.";
                         }
                     }
-                } else {
+                }
+                else
+                {
                     $error = $error . "Invalid username or password.";
                 }
-            } else {
+            }
+            else
+            {
                 echo "UwU, somethin went wong.";
             }
             $stmt->close();
