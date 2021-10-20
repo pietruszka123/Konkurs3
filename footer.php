c3d47c7aea4555128714d22d0cf812c9c344c4bd
SeKaFi69 <joker13313@interia.pl>
2021-10-20 20:33:12 +0200

    Revert "nie działało"
    This reverts commit bf35e602ad232ab27b2da692d2748d273b1f7f3d.

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
-
+</body2>
 </body>
 </html>
--- member.php
+++ member.php
@@ -6,76 +6,77 @@ include_once("header.php");
     exit();
 }*/
 ?>
+</body>
+<body2>
 <!-- marcin zostaw | nie -->
 <!-- o co chodzi? -->
-
-    <div class="grades element">
-        <div class="subjectGradesTable">
-            <?php
-            getUserGrades();
-            ?>
-        </div>
-    </div>
-    <div class="timeTable element">
-        <h1>Plan Lekcji</h1>
-        <div class="TimeTableE">
-
-            <?php
-            getTimetable();
-            ?>
-        </div>
-        <input type="submit" id="backward" value="<">
-        <input type="submit" id="reset" value="Today">
-        <input type="submit" id="forward" value=">">
-    </div>
-    <div>
-        <div class="daysUntilEndOfYear element">
-            <?php
-            getDaysUntilEndOfYear()
-            ?>
-        </div>
-
-
-        <div class="freeDaysTable element">
-            <h1>Najbliższe Dni Wolne</h1>
-            <?php
-            closestFreeDays()
-            ?>
-        </div>
-    </div>
-    <div class="closestExams element">
-        <h1>Najbliższe sprawdziany</h1>
+<div class="grades element">
+    <div class="subjectGradesTable">
         <?php
-        closestExams()
+        getUserGrades();
         ?>
     </div>
-    <div class="closestHomework element">
-        <h1>Zadania Domowe</h1>
-        <?php
-        closestHomework()
+</div>
+<div class="timeTable element">
+    <h1>Plan Lekcji</h1>
+    <div class="TimeTableE">
+
+          <?php
+        getTimetable();
         ?>
     </div>
-
-    <div class="attendanceTable element">
-        <h1>Obecności</h1>
-        <div class="attendanceTableE">
-            <?php
-            getAttendance($_SESSION["id"]);
-            ?>
-        </div>
-        <input type="submit" id="back" value="<">
-        <input type="submit" id="res" value="Today">
-        <input type="submit" id="for" value=">">
-    </div>
-
-    <div class="studentComments element">
+    <input type="submit" id="backward" value="<">
+    <input type="submit" id="reset" value="Today">
+    <input type="submit" id="forward" value=">">
+</div>
+<div>
+    <div class="daysUntilEndOfYear element">
         <?php
-        getUserComments($_SESSION["id"]);
+        getDaysUntilEndOfYear()
         ?>
     </div>
 
 
-    <script src="js/member.js"></script>
+    <div class="freeDaysTable element">
+        <h1>Najbliższe Dni Wolne</h1>
+        <?php
+        closestFreeDays()
+        ?>
+    </div>
+</div>
+<div class="closestExams element">
+    <h1>Najbliższe sprawdziany</h1>
+    <?php
+    closestExams()
+    ?>
+</div>
+<div class="closestHomework element">
+    <h1>Zadania Domowe</h1>
+    <?php
+    closestHomework()
+    ?>
+</div>
+
+<div class="attendanceTable element">
+    <h1>Obecności</h1>
+    <div class="attendanceTableE">
+    <?php
+    getAttendance($_SESSION["id"]);
+    ?>
+    </div>
+    <input type="submit" id="back" value="<">
+    <input type="submit" id="res" value="Today">
+    <input type="submit" id="for" value=">">
+</div>
+
+<div class="studentComments element">
+    <?php
+    getUserComments($_SESSION["id"]);
+    ?>
+</div>
+
+
+<script src="js/member.js"></script>
 
 
 <?php
