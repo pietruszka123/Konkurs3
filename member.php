7d2eedcbbf8720334104d62fbee7a2efa0588b2d
SeKaFi69 <joker13313@interia.pl>
2021-10-20 14:22:17 +0200

    cós

--- member.php
+++ member.php
@@ -17,30 +17,31 @@ include_once("header.php");
     </div>
 </div>
 <div class="timeTable element">
+    <h1>Plan Lekcji</h1>
     <div class="TimeTableE">
-        <h1>Plan Lekcji</h1>
-    <?php
-    getTimetable();
-    ?>
+
+          <?php
+        getTimetable();
+        ?>
     </div>
     <input type="submit" id="backward" value="<">
     <input type="submit" id="reset" value="Today">
     <input type="submit" id="forward" value=">">
 </div>
 <div>
-<div class="daysUntilEndOfYear element">
-    <?php
-    getDaysUntilEndOfYear()
-    ?>
-</div>
+    <div class="daysUntilEndOfYear element">
+        <?php
+        getDaysUntilEndOfYear()
+        ?>
+    </div>
 
 
-<div class="freeDaysTable element">
-    <h1>Najbliższe Dni Wolne</h1>
-    <?php
-    closestFreeDays()
-    ?>
-</div>
+    <div class="freeDaysTable element">
+        <h1>Najbliższe Dni Wolne</h1>
+        <?php
+        closestFreeDays()
+        ?>
+    </div>
 </div>
 <div class="closestExams element">
     <h1>Najbliższe sprawdziany</h1>
