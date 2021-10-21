5736e501552f0ca6407ed4b85b4d28c71412c64d
SeKaFi69 <joker13313@interia.pl>
2021-10-21 12:45:32 +0200

    pusz

--- teacher.php
+++ teacher.php
@@ -4,32 +4,13 @@ include_once("views/t_header.php");
 ?>
 <style>
     body {
-        display: flexbox;
-    }
-
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
         width: 100%;
-        height: 20%;
-    }
-
-    li {
-        display: inline-flex;
-        width: 20%;
-        height: 100%;
-        background-color: indigo;
+        height: 89vh;
     }
 
 </style>
-<ul class="HorizontalNavBar">
-    <li id="messages">wiadomosci</li>
-    <li id="grades">oceny</li>
-    <li id="?">cos tam</li>
-    <li id="??">cos tam jeszcze</li>
-</ul>
-<iframe src="views/grades.php" frameborder="0"></iframe>
+
+<iframe src="views/grades.php" frameborder="0" class="inframe element"></iframe>
 <script src="js/teacher.js"></script>
 <?php
 include_once("footer.php");
