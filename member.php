30a438642ba611fe859ce46ecb68e54e00f125f3
SeKaFi69 <joker13313@interia.pl>
2021-10-24 10:44:46 +0200

    s

--- member.php
+++ member.php
@@ -12,79 +12,77 @@ include_once("header.php");
 
 
 <gridzio>
-<div class="grades element">
-    <div class="subjectGradesTable">
-        <?php
-        getUserGrades();
-        ?>
+    <div class="grades element">
+        <div class="subjectGradesTable">
+            <?php
+            getUserGrades();
+            ?>
+        </div>
     </div>
-</div>
-<div class="timeTable element">
-    <h1>Plan Lekcji</h1>
-    <div class="TimeTableE">
+    <div class="timeTable element">
+        <h1>Plan Lekcji</h1>
+        <div class="TimeTableE">
 
-          <?php
-        getTimetable();
-        ?>
+            <?php
+            getTimetable();
+            ?>
+        </div>
+        <div class='strzalkiZmienne T'>
+            <input type="submit" id="backward" value="<">
+            <input type="submit" id="reset" value="Today">
+            <input type="submit" id="forward" value=">">
+        </div>
     </div>
-    <div class='strzalkiZmienne T'>
-        <input type="submit" id="backward" value="<">
-        <input type="submit" id="reset" value="Today">
-        <input type="submit" id="forward" value=">">
+    <div class="dwa">
+        <div class="daysUntilEndOfYear element">
+            <?php
+            getDaysUntilEndOfYear()
+            ?>
+        </div>
+        <div class="luckyNumbers element">
+            <?php
+            getLuckyNumber();
+            ?>
+        </div>
     </div>
-</div>
-<div>
-    <div class="daysUntilEndOfYear element">
-        <?php
-        getDaysUntilEndOfYear()
-        ?>
-    </div>
-
-
     <div class="freeDaysTable element">
         <h1>Najbliższe Dni Wolne</h1>
         <?php
         closestFreeDays()
         ?>
     </div>
-</div>
-<div class="luckyNumbers element">
-    <?php
-        getLuckyNumber();
-    ?>
-</div>
-<div class="closestExams element">
-    <h1>Najbliższe sprawdziany</h1>
-    <?php
-    closestExams()
-    ?>
-</div>
-<div class="closestHomework element">
-    <h1>Zadania Domowe</h1>
-    <?php
-    closestHomework()
-    ?>
-</div>
-
-<div class="attendanceTable element">
-    <h1>Obecności</h1>
-    <div class="attendanceTableE">
-    <?php
-    getAttendance($_SESSION["id"]);
-    ?>
+    <div class="closestExams element">
+        <h1>Najbliższe sprawdziany</h1>
+        <?php
+        closestExams()
+        ?>
     </div>
-    <div class='strzalkiZmienne A'>
-    <input type='submit' id='back' value='<'>
-    <input type='submit' id='res' value='Today'>
-    <input type='submit' id='for' value='>'>
+    <div class="closestHomework element">
+        <h1>Zadania Domowe</h1>
+        <?php
+        closestHomework()
+        ?>
     </div>
-</div>
 
-<div class="studentComments element">
-    <?php
-    getUserComments($_SESSION["id"]);
-    ?>
-</div>
+    <div class="attendanceTable element">
+        <h1>Obecności</h1>
+        <div class="attendanceTableE">
+            <?php
+            getAttendance($_SESSION["id"]);
+            ?>
+        </div>
+        <div class='strzalkiZmienne A'>
+            <input type='submit' id='back' value='<'>
+            <input type='submit' id='res' value='Today'>
+            <input type='submit' id='for' value='>'>
+        </div>
+    </div>
+
+    <div class="studentComments element">
+        <?php
+        getUserComments($_SESSION["id"]);
+        ?>
+    </div>
 </gridzio>
 
 <script src="js/member.js"></script>
--- messages.php
+++ messages.php
@@ -1,55 +1,55 @@
 <?php
 include_once "header.php";
 ?>
+<div class="bg">
+    <input type="button" id="Switch" value="Toggle">
 
-<input type="button" id="Switch" value="Toggle">
-
-<input type="button" value="Odświerz" id="refresh">
-<div class="messageForm">
-    <input type="hidden" name="viewMessage" value="messageId" />
-    <div class="messagesButtons">
-        <?php
-        getUserMessages();
-        ?>
+    <input type="button" value="Odświerz" id="refresh">
+    <div class="messageForm">
+        <input type="hidden" name="viewMessage" value="messageId" />
+        <div class="messagesButtons">
+            <?php
+            getUserMessages();
+            ?>
+        </div>
     </div>
-</div>
-<div class="messageContent">
-</div>
-<div class="sendMessageBox">
-    <label for="selectReceiver">Choose a car:</label>
+    <div class="messageContent">
+    </div>
+    <div class="sendMessageBox">
+        <label for="selectReceiver">Choose a car:</label>
 
-    <!-- <select name="selectReceiver" id="selectReceiver">
+        <!-- <select name="selectReceiver" id="selectReceiver">
         <?php
         //viewAllReceivers();
         ?>
     </select> -->
-    <input type="button" id="selectReciver" value="Choose cars to send">
-    <input type="text" id="titleInputBox" name="titleInputBox">
-    <input type="text" id="messageInputContent" name="messageInputContent">
-    <input type="button" class="sendMessageButton" value="wysjil">
-</div>
-<script src="js/messages.js" type="module"></script>
-<div id="popUp" class="popUp">
-    <div class="popUp-content">
-        <span class="popUp-close">&times;</span>
-        <div>
-            <select id="selectReceiverType">
-                <option value="uczen">a</option>
-                <option value="nauczyciel">b</option>
-            </select>
+        <input type="button" id="selectReciver" value="Choose cars to send">
+        <input type="text" id="titleInputBox" name="titleInputBox">
+        <input type="text" id="messageInputContent" name="messageInputContent">
+        <input type="button" class="sendMessageButton" value="wysjil">
+    </div>
+    <script src="js/messages.js" type="module"></script>
+    <div id="popUp" class="popUp">
+        <div class="popUp-content">
+            <span class="popUp-close">&times;</span>
+            <div>
+                <select id="selectReceiverType">
+                    <option value="uczen">a</option>
+                    <option value="nauczyciel">b</option>
+                </select>
 
-            <div class="notSelected">
-                <?php
-                viewAllReceivers();
-                ?>
+                <div class="notSelected">
+                    <?php
+                    viewAllReceivers();
+                    ?>
+                </div>
+                <div class="Selected">
+                </div>
+                <input id="subbmit" type="button" value="zatwierdź wybrane samochody">
             </div>
-            <div class="Selected">
-            </div>
-            <input id="subbmit" type="button" value="zatwierdź wybrane samochody">
         </div>
     </div>
 </div>
-
 
 <?php
 include_once "footer.php";
